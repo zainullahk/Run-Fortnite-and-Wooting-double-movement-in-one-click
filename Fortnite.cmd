e5d2a5883bd980ed9ddb5681eb664dc856f863de
zainullahk <72628804+zainullahk@users.noreply.github.com>
2021-05-16 12:34:22 +0400

    Update Fortnite.cmd

--- Fortnite.cmd
+++ Fortnite.cmd
@@ -1,3 +1,3 @@
 "C:\Program Files\Epic Games\Fortnite\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping.exe"
-start "Wooting" "C:\Users\Zainullah\AppData\Local\Programs\wooting-double-movement\wooting-double-movement.exe" 
+start "Wooting" "C:\%HOMEPATH%\AppData\Local\Programs\wooting-double-movement\wooting-double-movement.exe" 
 exit
